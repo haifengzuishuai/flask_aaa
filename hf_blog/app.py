00408b545939a485c1479738b7c8c86919bc4546
haifeng <dada0614@126.com>
2019-08-24 09:54:10 +0800

    写好登录页面和简单的登录，未进行到数据库校验

--- hf_blog/app.py
+++ hf_blog/app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+# encoding:utf-8
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'hallow haifeng!'
+
+
+@app.route('/login', methods=['post','get'])
+def login():
+    if request.method=='GET':
+        return render_template('login.html')
+    else:
+        name = request.form.get('name')
+        password = request.form.get('password')
+        print(name)
+        print(password)
+        if name == '1' and password == '1':
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html')
+
+
+@app.route('/catalogue')
+def catalogue():
+    return '返回所有的博客内容'
+
+
+@app.route('/catalogue/<id>')
+def article(id):
+    return '您请求的文章是%s' % id
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
