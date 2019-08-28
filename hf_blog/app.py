44f1c80a0c1b006d7cd916625e63e4e7ebcc2a03
haifeng <dada0614@126.com>
2019-08-28 08:41:23 +0800

    更新index

--- hf_blog/app.py
+++ hf_blog/app.py
@@ -9,9 +9,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login', methods=['post','get'])
+@app.route('/login', methods=['post', 'get'])
 def login():
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('login.html')
     else:
         name = request.form.get('username')
@@ -23,6 +23,13 @@ def login():
         else:
             return render_template('login.html')
 
+@app.route('/regist',methods=['post', 'get'])
+def regist():
+    phone = request.form.get('phone')
+    password = request.form.get('password')
+    password1 = request.form.get('password1')
+
+    return render_template('regist.html')
 
 @app.route('/catalogue')
 def catalogue():
@@ -35,4 +42,4 @@ def article(id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0' ,port=5000 ,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
